d3f03c448423fd7f96e32ff52cdf30bbdba06a76
DailmaranF0x bc2025-2 <usichenko.1608@gmail.com>
2025-09-28 22:18:53 +0300

    Додано конструювання URL для JPY на 27.01.2024 у main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// main.js
+
+const currency = "JPY";
+const exact_date = "20240127";
+
+const url = new URL("/NBUStatService/v1/statdirectory/exchange", "https://bank.gov.ua/");
+
+url.searchParams.append("json", "");
+url.searchParams.append("valcode", currency);
+url.searchParams.append("date", exact_date);
+
+console.log(url.toString());
+
